c344b2f9d719380b0a23480af0c0fcd1cd19451b
bocyej <jacobkristopherlaurel@gmail.com>
2025-05-25 01:09:59 +0800

    add change theme functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    // Toggle Theme Switch
+    const toggleThemeButton = document.getElementById("toggleThemeButton");
+
+    function isDarkThemeActive() {
+        return document.body.classList.contains("dark-mode");
+    }
+
+    function switchTheme() {
+        const isDarkTheme = isDarkThemeActive();
+
+        const headerLogo = document.getElementById("header-logo");
+        const toggleThemeLogo = document.getElementById("toggle-theme-logo");
+
+        headerLogo.src = isDarkTheme
+            ? "../assets/photos/logo-dark.png"
+            : "../assets/photos/logo-light.png";
+
+        toggleThemeLogo.src = isDarkTheme
+            ? "../assets/photos/dark-mode.png"
+            : "../assets/photos/light-mode.png";
+
+        const path = document.querySelectorAll(".icon-path");
+        path.forEach(path => {
+            path.setAttribute("fill", isDarkTheme ? "#313638" : "#E8E9EB");
+        });
+    }
+
+    toggleThemeButton.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        document.body.classList.toggle("light-mode");
+        switchTheme();
+    });
+
+
     // Entry for each word
     const paragraphs = document.querySelectorAll(".introduction-description p");
 
@@ -25,6 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
         totalDelay += words.length * 0.03 + 0.3;
     });
 
+
     // Right Arrow Bouncing
     const arrow = document.getElementById("arrow");
 
@@ -33,4 +68,8 @@ document.addEventListener("DOMContentLoaded", () => {
         void arrow.offsetWidth;
         arrow.classList.add("bounce");
     }, 2500);
+
+
+    // Ensure dark theme is selected on page load
+    switchTheme();
 });
