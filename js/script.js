6d8c7e057e1f64d0033a4dcb744be4daa9c259e2
bocyej <jacobkristopherlaurel@gmail.com>
2025-06-05 02:09:36 +0800

    add defensive programming technique

--- js/script.js
+++ js/script.js
@@ -12,14 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const headerLogo = document.getElementById("header-logo");
         const toggleThemeLogo = document.getElementById("toggle-theme-logo");
-        const header1 = document.querySelectorAll("h1");
-        const header2 = document.querySelectorAll("h2");
-        const header3 = document.querySelectorAll("h3");
-        const paragraphs = document.querySelectorAll("p");
-        const aTag = document.querySelectorAll("a");
-        const lists = document.querySelectorAll("li");
-
-        const iconPath = document.getElementsByClassName(".icon-path");
 
         if (window.location.href.includes("/about") || window.location.href.includes("/contact") || window.location.href.includes("/projects")) {
             headerLogo.src = isDarkTheme
@@ -39,13 +31,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 : "assets/photos/dark-mode.png";
         }
 
-        const activeNavbarElement = document.querySelector(".active");
-        activeNavbarElement.style.borderBottom = isDarkTheme ? "#E8E9EB 4px solid" : "#313638 4px solid";
-
-        const backgroundRectangle = document.querySelector(".background-rectangle");
-        backgroundRectangle.style.backgroundColor = isDarkTheme ? "#4C5153" : "#E0DFD5";
+        const header1 = document.querySelectorAll("h1");
+        const header2 = document.querySelectorAll("h2");
+        const header3 = document.querySelectorAll("h3");
+        const paragraphs = document.querySelectorAll("p");
+        const aTag = document.querySelectorAll("a");
+        const lists = document.querySelectorAll("li");
 
         const style = document.createElement('style');
+
+        const projectCard = document.querySelectorAll(".card-text");
 
         if (isDarkTheme) {
             aTag.forEach(link => {
@@ -58,9 +53,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 link.classList.add('dark-mode');
             });
 
+            projectCard.forEach(link => {
+                link.classList.remove('card-description-color-light');
+                link.classList.add('card-description-color-dark');
+            });
+
             style.innerHTML = `
                 .timeline-circle::before {
-                    background-color: #4C5153 !important;
+                    background-color: #313638 !important;
                     border: 3px solid #E8E9EB;
                 }
             `;
@@ -75,22 +75,31 @@ document.addEventListener("DOMContentLoaded", () => {
                 link.classList.remove('dark-mode');
             });
 
+            projectCard.forEach(link => {
+                link.classList.add('card-description-color-light');
+                link.classList.remove('card-description-color-dark');
+            });
+
             style.innerHTML = `
                 .timeline-circle::before {
-                    background-color: #E0DFD5 !important;
+                    background-color: #E8E9EB !important;
                     border: 3px solid #313638;
                 }
             `;
         }
         document.head.appendChild(style);
 
+        const activeNavbarElement = document.querySelector(".active");
+        activeNavbarElement.style.borderBottom = isDarkTheme ? "#E8E9EB 4px solid" : "#313638 4px solid";
+
+        // const backgroundRectangle = document.querySelector(".background-rectangle");
+        // backgroundRectangle.style.backgroundColor = isDarkTheme ? "#4C5153" : "#E0DFD5";
+
         const timeline = document.querySelector(".timeline");
-        timeline.style.borderLeft = isDarkTheme ? "2px solid #E8E9EB" : "2px solid #313638";
 
-        iconPath.forEach(path => {
-            path.setAttribute("fill", isDarkTheme ? "#E8E9EB" : "#313638");
-        });
-
+        if (timeline) {
+            timeline.style.borderLeft = isDarkTheme ? "2px solid #E8E9EB" : "2px solid #313638";
+        }
     }
 
     toggleThemeButton.addEventListener("click", () => {
@@ -105,35 +114,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let totalDelay = 0;
 
-    paragraphs.forEach((p, paraIndex) => {
-        const text = p.textContent.trim();
-        const words = text.split(" ");
-        p.innerHTML = "";
+    if (paragraphs) {
+        paragraphs.forEach((p, paraIndex) => {
+            const text = p.textContent.trim();
+            const words = text.split(" ");
+            p.innerHTML = "";
 
-        words.forEach((word, wordIndex) => {
-            const span = document.createElement("span");
-            span.textContent = word;
-            span.classList.add("word");
+            words.forEach((word, wordIndex) => {
+                const span = document.createElement("span");
+                span.textContent = word;
+                span.classList.add("word");
 
-            const delay = totalDelay + wordIndex * 0.03;
-            span.style.animationDelay = `${delay}s`;
+                const delay = totalDelay + wordIndex * 0.03;
+                span.style.animationDelay = `${delay}s`;
 
-            p.appendChild(span);
-            p.appendChild(document.createTextNode(" "));
+                p.appendChild(span);
+                p.appendChild(document.createTextNode(" "));
+            });
+
+            totalDelay += words.length * 0.03 + 0.3;
         });
-
-        totalDelay += words.length * 0.03 + 0.3;
-    });
+    }
 
 
     // Right Arrow Bouncing
     const arrow = document.getElementById("arrow");
-
-    setInterval(() => {
-        arrow.classList.remove("bounce");
-        void arrow.offsetWidth;
-        arrow.classList.add("bounce");
-    }, 2500);
+    
+    if (arrow) {
+        setInterval(() => {
+            arrow.classList.remove("bounce");
+            void arrow.offsetWidth;
+            arrow.classList.add("bounce");
+        }, 2500);
+    }
 
 
     // Ensure dark theme is selected on page load
