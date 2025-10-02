305f4aeaf8b5d2c07cf9d57a6194ff648b6c5cef
bocyej <jacobkristopherlaurel@gmail.com>
2025-10-03 04:27:55 +0800

    create header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,51 +1,62 @@
-import { useState } from "react";
 import "../css/Header.css";
+import headerLogo from "../assets/photos/logo-light.png";
+import toggleLogo from "../assets/photos/light-mode.png";
+import { switchTheme } from "../utils/script";
 
 function Header() {
   function showMobileNavModal() {}
 
+  const handleToggleTheme = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+    console.log("working in ts");
+    switchTheme();
+  }
+
   return (
     <>
-      <nav className="limited-width">
-        <a href="index.html">
-          <img
-            id="header-logo"
-            src="../assets/photos/logo-light.png"
-            alt="Logo"
-            className="header-logo"
-          />
-        </a>
-        <a href="#" onClick={showMobileNavModal} className="hamburger">
-          <i className="fa fa-bars"></i>
-        </a>
-        <ul>
-          <li>
-            <a href="about.html" className="nav-link underline">
-              // About
-            </a>
-          </li>
-          <li>
-            <a href="projects.html" className="nav-link underline">
-              // Projects
-            </a>
-          </li>
-          <li>
-            <a href="contact.html" className="nav-link underline">
-              // Contact
-            </a>
-          </li>
-        </ul>
-        <a id="themeButton">
-          <div id="toggleThemeButton">
+      <header>
+        <nav className="limited-width">
+          <a href="index.html">
             <img
-              id="toggle-theme-logo"
-              src="../assets/photos/light-mode.png"
-              alt="Toggle Theme"
-              className="toggle-theme-logo"
+              id="header-logo"
+              src={headerLogo}
+              alt="Logo"
+              className="header-logo"
             />
-          </div>
-        </a>
-      </nav>
+          </a>
+          <a href="#" onClick={showMobileNavModal} className="hamburger">
+            <i className="fa fa-bars"></i>
+          </a>
+          <ul>
+            <li>
+              <a href="#about" className="nav-link underline">
+                // About
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="nav-link underline">
+                // Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="nav-link underline">
+                // Contact
+              </a>
+            </li>
+          </ul>
+          <a id="themeButton" onClick={handleToggleTheme}>
+            <div id="toggleThemeButton">
+              <img
+                id="toggle-theme-logo"
+                src={toggleLogo}
+                alt="Toggle Theme"
+                className="toggle-theme-logo"
+              />
+            </div>
+          </a>
+        </nav>
+      </header>
     </>
   );
 }
