d544e3f83eb9aaf53c17180257af111d994b4c38
bocyej <jacobkristopherlaurel@gmail.com>
2025-10-07 02:53:34 +0800

    use component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,27 +2,35 @@ import "../css/Footer.css";
 import GitHubIcon from "./icons/Github";
 import LinkedinIcon from "./icons/Linkedin";
 import InstagramIcon from "./icons/Instagram";
+import Copyright from "./Copyright";
+import { useEffect } from "react";
+import { switchTheme } from "../utils/script";
+import SocialMediaButton from "./SocialMediaButton";
 
 function Footer() {
+  useEffect(() => {
+    switchTheme();
+  });
+
   return (
     <>
       <footer>
         <section>
           <div className="limited-width">
-            <p>Copyright&copy; 2025 by Jacob Laurel. All Rights Reserved.</p>
+            <Copyright />
             <div className="footer-social-media">
-              <a
-                href="https://www.linkedin.com/in/laureljacob/"
-                target="_blank"
-              >
-                <LinkedinIcon />
-              </a>
-              <a href="https://github.com/bocyej" target="_blank">
-                <GitHubIcon />
-              </a>
-              <a href="https://www.instagram.com/bocyej/" target="_blank">
-                <InstagramIcon />
-              </a>
+              <SocialMediaButton
+                link="https://www.linkedin.com/in/laureljacob/"
+                icon={<LinkedinIcon />}
+              />
+              <SocialMediaButton
+                link="https://github.com/bocyej/"
+                icon={<GitHubIcon />}
+              />
+              <SocialMediaButton
+                link="https://www.instagram.com/bocyej/"
+                icon={<InstagramIcon />}
+              />
             </div>
           </div>
         </section>
