9c0b0360a76be821d3312907db4d2e4bf705899c
bocyej <jacobkristopherlaurel@gmail.com>
2025-10-07 02:52:25 +0800

    create divider component

---
+++ src/components/Divider.tsx
@@ -0,0 +1,12 @@
+const Divider = () => {
+  return (
+    <>
+      <div className="half-screen">
+        <div className="horizontal-divider"></div>
+      </div>
+    </>
+  );
+};
+
+
+export default Divider;
