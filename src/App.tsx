d613520a45f93a53c8695e4dc8463b7231a00a21
bocyej <jacobkristopherlaurel@gmail.com>
2025-11-07 03:28:01 +0800

    add click spark component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header.tsx'
-import Body from './components/Body.tsx'
-import Footer from './components/Footer.tsx'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header.tsx";
+import Body from "./components/Body.tsx";
+import Footer from "./components/Footer.tsx";
+import ClickSpark from "./components/reactbits/ClickSpark.tsx";
 
 function App() {
-
   return (
     <>
-      <Header />
-      <Body />
-      <Footer />
+      <ClickSpark
+        sparkColor="#fff"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        <Header />
+        <Body />
+        <Footer />
+      </ClickSpark>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
