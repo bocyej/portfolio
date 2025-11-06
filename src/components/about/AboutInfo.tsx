f384dd5112407aad4f66c7f4001859c8acad0622
bocyej <jacobkristopherlaurel@gmail.com>
2025-11-07 03:26:56 +0800

    add class props

--- src/components/about/AboutInfo.tsx
+++ src/components/about/AboutInfo.tsx
@@ -3,16 +3,15 @@ import type { ReactNode } from "react";
 interface Props {
   title: string;
   content: ReactNode;
+  additionalClass: string;
 }
 
-const AboutInfo = ({ title, content }: Props) => {
+const AboutInfo = ({ title, content, additionalClass }: Props) => {
   return (
     <>
-      <div className="limited-width">
-        <div className="half-screen">
-          <section className="label">{title}</section>
-          <section className="background-rectangle">{content}</section>
-        </div>
+      <div className={`section-border ${additionalClass}`}>
+        <section className="label">{title} <a href="/certifications">LOL</a></section>
+        <section className="background-rectangle">{content}</section>
       </div>
     </>
   );
