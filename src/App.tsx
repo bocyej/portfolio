6b7a217d02c0d79b830fe6564c46432643cec37a
bocyej <jacobkristopherlaurel@gmail.com>
2025-11-08 01:35:52 +0800

    add loading page

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,27 @@ import Header from "./components/Header.tsx";
 import Body from "./components/Body.tsx";
 import Footer from "./components/Footer.tsx";
 import ClickSpark from "./components/reactbits/ClickSpark.tsx";
+import LoadingPage from "./components/LoadingPage.tsx";
+import "./css/component-styles/LoadingPage.css"
 
 function App() {
   return (
     <>
-      <ClickSpark
-        sparkColor="#fff"
-        sparkSize={10}
-        sparkRadius={15}
-        sparkCount={8}
-        duration={400}
-      >
-        <Header />
-        <Body />
-        <Footer />
-      </ClickSpark>
+      <LoadingPage />
+
+      <div className="container">
+        <ClickSpark
+          sparkColor="#fff"
+          sparkSize={10}
+          sparkRadius={15}
+          sparkCount={8}
+          duration={400}
+        >
+          <Header />
+          <Body />
+          <Footer />
+        </ClickSpark>
+      </div>
     </>
   );
 }
