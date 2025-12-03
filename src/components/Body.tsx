5311eb77534ae791b4ce783f7223eeb73677f69d
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-04 02:43:41 +0800

    remove unused import

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,7 +2,6 @@ import "../css/Body.css";
 import "../assets/photos/light-mode.png";
 import { useEffect } from "react";
 import { introductionAnimation, switchTheme } from "../utils/script";
-import About from "./pageSections/About";
 import Contact from "./pageSections/Contact";
 import Home from "./pageSections/Home";
 import MobileNavBar from "./MobileNavbar";
