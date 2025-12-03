77e76c66017c278116647dc535bacbce98048a99
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-04 02:39:47 +0800

    add initial loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,40 @@ import Body from "./components/Body.tsx";
 import Footer from "./components/Footer.tsx";
 import ClickSpark from "./components/reactbits/ClickSpark.tsx";
 import LoadingPage from "./components/LoadingPage.tsx";
-import "./css/component-styles/LoadingPage.css"
+import "./css/component-styles/LoadingPage.css";
+import { useLayoutEffect, useState } from "react";
 
 function App() {
+  const [showLoading, setShowLoading] = useState(false);
+  const [isContentVisible, setIsContentVisible] = useState(false);
+
+  useLayoutEffect(() => {
+    const loadingShown = sessionStorage.getItem("loadingShown");
+
+    if (!loadingShown) {
+      setShowLoading(true);
+      sessionStorage.setItem("loadingShown", "true");
+    } else {
+      setIsContentVisible(true);
+    }
+  }, []);
+
+  const handleLoadingComplete = () => {
+    setShowLoading(false);
+    setTimeout(() => setIsContentVisible(true), 200);
+  };
+
   return (
     <>
-      <LoadingPage />
+      {showLoading && <LoadingPage onComplete={handleLoadingComplete} />}
 
-      <div className="container">
+      <div
+        className="container"
+        style={{
+          opacity: isContentVisible ? 1 : 0,
+          transition: "opacity 0.8s ease",
+        }}
+      >
         <ClickSpark
           sparkColor="#fff"
           sparkSize={10}
