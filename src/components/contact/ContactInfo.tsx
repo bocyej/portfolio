7ae5da7d9693ad1e740c8e2c9a0ebb316b69445c
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-04 02:44:05 +0800

    create contact info

---
+++ src/components/contact/ContactInfo.tsx
@@ -0,0 +1,16 @@
+import ContactCard from "../contact/ContactCard";
+import GitHubIcon from "../icons/Github";
+import InstagramIcon from "../icons/Instagram";
+import LinkedinIcon from "../icons/Linkedin";
+
+const ContactInfo = () => {
+  return (
+    <>
+      <div id="contact">
+        <ContactInfo />
+      </div>
+    </>
+  );
+};
+
+export default ContactInfo;
