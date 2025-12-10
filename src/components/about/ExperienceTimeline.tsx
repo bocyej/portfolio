9bef9ce5cb40c730184ccb62f5e1335312e655c2
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-11 00:02:52 +0800

    add backbutton + center container

--- src/components/about/ExperienceTimeline.tsx
+++ src/components/about/ExperienceTimeline.tsx
@@ -1,34 +1,42 @@
 import ExperienceCircle from "./ExperienceCircle";
+import "../../index.css";
+import "../../css/Body.css";
+import BackButton from "../BackButton";
 
 const ExperienceTimeline = () => {
   return (
     <>
-      <section className="background-rectangle">
-        <article className="timeline">
-          <ul>
-            <ExperienceCircle
-              company={"Rakso Computer Technology Inc."}
-              role={"Intern, Research and Design"}
-              duration={"January 2025 - April 2025"}
-            />
-            <ExperienceCircle
-              company={"Serbiz Inc."}
-              role={"Contractual, Full-Stack Flutter Developer"}
-              duration={"January 2025 - March 2025"}
-            />
-            <ExperienceCircle
-              company={"Freelance Web Developer"}
-              role={""}
-              duration={"July 2023 - April 2025"}
-            />
-            <ExperienceCircle
-              company={"St. Paul University Manila"}
-              role={"Contractual, Full-Stack Web Developer"}
-              duration={"April 2023 - May 2024"}
-            />
-          </ul>
-        </article>
-      </section>
+      <BackButton title={"Experiences"} />
+      <div className="center-horizontally">
+        <div></div>
+        <section className="background-rectangle">
+          <article className="timeline">
+            <ul>
+              <ExperienceCircle
+                company={"Rakso Computer Technology Inc."}
+                role={"Intern, Research and Design"}
+                duration={"January 2025 - April 2025"}
+              />
+              <ExperienceCircle
+                company={"Serbiz Inc."}
+                role={"Contractual, Full-Stack Flutter Developer"}
+                duration={"January 2025 - March 2025"}
+              />
+              <ExperienceCircle
+                company={"Freelance Web Developer"}
+                role={""}
+                duration={"July 2023 - April 2025"}
+              />
+              <ExperienceCircle
+                company={"St. Paul University Manila"}
+                role={"Contractual, Full-Stack Web Developer"}
+                duration={"April 2023 - May 2024"}
+              />
+            </ul>
+          </article>
+        </section>
+        <div></div>
+      </div>
     </>
   );
 };
