3d364381a83a277150865d3a05c9e154208d6687
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-11 00:02:22 +0800

    add title

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,13 +1,23 @@
 import ArrowBack from "./icons/ArrowBack";
+import "../index.css";
+import "../css/Body.css";
 
-function BackButton() {
+interface Props {
+  title: string;
+}
+
+function BackButton({ title }: Props) {
   return (
     <>
-      <a href={"/"} className="go-back">
-        <ArrowBack />
+      <div className="center-horizontally title-spacing">
+        <a href={"/"} className="go-back">
+          <ArrowBack />
 
-        <p>Back to Home</p>
-      </a>
+          <p>Back to Home</p>
+        </a>
+        <p className="sub-page-title">{title}</p>
+        <div className="right-box"></div>
+      </div>
     </>
   );
 }
