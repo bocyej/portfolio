3201c9985913fd0f62e83bdf6598f664ed5af3bd
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-10 22:50:44 +0800

    create arrow back icon

---
+++ src/components/icons/ArrowBack.tsx
@@ -0,0 +1,18 @@
+import "../../css/Body.css";
+
+function InstagramIcon() {
+  return (
+    <>
+      <svg fill="none" stroke="var(--color)" viewBox="0 0 24 24" width="50" height="50">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1"
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        ></path>
+      </svg>
+    </>
+  );
+}
+
+export default InstagramIcon;
