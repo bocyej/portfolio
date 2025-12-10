536a2d00a323e531c38ed7266116f9260da422e2
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-11 00:03:33 +0800

    add backbutton

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,15 +1,19 @@
+import BackButton from "../BackButton";
 import CollaborationProjectsList from "./CollaborationProjectsList";
 import SoloProjectsList from "./SoloProjectsList";
+import "../../index.css";
+import "../../css/Body.css";
 
 const ViewAllProjects = () => {
   return (
     <>
+      <section>
+        <BackButton title={""} />
+      </section>
       <div id="projects">
-        
         <SoloProjectsList />
 
         <CollaborationProjectsList />
-        
       </div>
     </>
   );
