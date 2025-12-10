c08ba108989158dca3a77c202408d1d3e099c2ba
bocyej <jacobkristopherlaurel@gmail.com>
2025-12-11 00:04:01 +0800

    create timeline summary (certifications and exxperiences)

---
+++ src/components/about/ExperienceTimelineSummary.tsx
@@ -0,0 +1,38 @@
+import ExperienceCircle from "./ExperienceCircle";
+import "../../index.css";
+import "../../css/Body.css";
+
+const ExperienceTimelineSummary = () => {
+  return (
+    <>
+      <section className="background-rectangle">
+        <article className="timeline">
+          <ul>
+            <ExperienceCircle
+              company={"Rakso Computer Technology Inc."}
+              role={"Intern, Research and Design"}
+              duration={"January 2025 - April 2025"}
+            />
+            <ExperienceCircle
+              company={"Serbiz Inc."}
+              role={"Contractual, Full-Stack Flutter Developer"}
+              duration={"January 2025 - March 2025"}
+            />
+            <ExperienceCircle
+              company={"Freelance Web Developer"}
+              role={""}
+              duration={"July 2023 - April 2025"}
+            />
+            <ExperienceCircle
+              company={"St. Paul University Manila"}
+              role={"Contractual, Full-Stack Web Developer"}
+              duration={"April 2023 - May 2024"}
+            />
+          </ul>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default ExperienceTimelineSummary;
