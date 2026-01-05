9d1aa6f9b46e6a8c8c21872f8baed0557fbe49c2
bocyej <jacobkristopherlaurel@gmail.com>
2026-01-06 04:07:56 +0800

    fix icon color, change theme functionality, scroll to area

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,38 +1,78 @@
+import { useEffect } from "react";
 import mobileToggleLogo from "../assets/photos/light-mode.png";
-import { hideMobileNavModal, } from "../utils/script";
+import { hideMobileNavModal, switchTheme } from "../utils/script";
 import CloseIcon from "./icons/Close";
 
 const MobileNavBar = () => {
+  useEffect(() => {
+    switchTheme();
+  });
+
+  const handleToggleTheme = () => {
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+    switchTheme();
+  };
+
   const toggleMobileModal = () => {
     hideMobileNavModal();
+  };
+
+  const scrollToAboutArea = () => {
+    const scrollTarget = document.getElementById("about");
+
+    if (scrollTarget) {
+      const scrollOffset = scrollTarget.getBoundingClientRect();
+      const topElement = scrollOffset.top + window.scrollY;
+      const scrollToPosition = topElement - 25;
+
+      window.scrollTo({
+        top: scrollToPosition,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollToContactArea = () => {
+    const scrollTarget = document.getElementById("contact");
+
+    if (scrollTarget) {
+      const scrollOffset = scrollTarget.getBoundingClientRect();
+      const topElement = scrollOffset.top + window.scrollY;
+      const scrollToPosition = topElement - 25;
+
+      window.scrollTo({
+        top: scrollToPosition,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
     <>
       <div id="mobile-nav-modal">
         <div className="modal-content">
-          <a href="#" onClick={toggleMobileModal} className="close-icon">
+          <a onClick={toggleMobileModal} className="close-icon">
             <CloseIcon />
           </a>
           <ul>
             <li>
-              <a href="#about">// About</a>
+              <a onClick={scrollToAboutArea}>// About</a>
             </li>
             <li>
-              <a href="#projects">// Projects</a>
+              <a onClick={scrollToContactArea}>// Contact</a>
             </li>
             <li>
-              <a href="#contact">// Contact</a>
-            </li>
-            <li>
-              <div id="toggleThemeButtonModal">
-                <img
-                  id="toggle-theme-logo"
-                  src={mobileToggleLogo}
-                  alt="Toggle Theme"
-                  className="toggle-theme-logo-modal"
-                />
-              </div>
+              <a onClick={handleToggleTheme}>
+                <div id="toggleThemeButtonModal">
+                  <img
+                    id="toggle-theme-logo"
+                    src={mobileToggleLogo}
+                    alt="Toggle Theme"
+                    className="toggle-theme-logo-modal"
+                  />
+                </div>
+              </a>
             </li>
           </ul>
         </div>
