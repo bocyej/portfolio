8dfc69f6e30c2af49abcb75662674140e9363211
bocyej <jacobkristopherlaurel@gmail.com>
2026-01-07 03:36:38 +0800

    fikx svg camel case typo

--- src/components/icons/ArrowBack.tsx
+++ src/components/icons/ArrowBack.tsx
@@ -6,9 +6,9 @@ function ArrowBack() {
     <>
       <svg fill="none" stroke="var(--color)" viewBox="0 0 24 24" width="30" height="30">
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
           d="M10 19l-7-7m0 0l7-7m-7 7h18"
         ></path>
       </svg>
