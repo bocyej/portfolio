ffe79f9439085c1c1da0c179bd1c93ed7fb9e35e
bocyej <jacobkristopherlaurel@gmail.com>
2026-01-08 23:46:41 +0800

    add scroll animation when navigating pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./utils/ScrollToTop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
